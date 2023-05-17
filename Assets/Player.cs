cc7d1e52d7de47356d532ef8e25e2408fca4681d
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2023-05-16 22:59:35 -0400

    fix(Fisicas): CollisionBox de las plataformas y script del player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Animator animator;
+    public SpriteRenderer spriterenderer;
     public float velocidad = 0.01f;
+    public int coins = 0;
 
 
-
+    public AudioSource coinSound;
+    public AudioClip OtherSound;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //coinSound = GetComponent<>
     }
 
     // Update is called once per frame
@@ -21,20 +24,24 @@ public class Player : MonoBehaviour
     {
         //Movimiento a la izquierda
         if (Input.GetKey(KeyCode.LeftArrow)) {
+            spriterenderer.flipX = true;
             transform.Translate(-velocidad, 0, 0);
-            animator.SetBool("Izquierda", true);
+            animator.SetBool("Derecha", true);
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow)) {
-            animator.SetBool("Izquierda", false);
+            animator.SetBool("Derecha", false);
         }
         //Movimiento a la derecha
         if (Input.GetKey(KeyCode.RightArrow)) {
+            spriterenderer.flipX = false;
             transform.Translate(velocidad, 0, 0);
             animator.SetBool("Derecha", true);
         }
+
         if (Input.GetKeyUp(KeyCode.RightArrow)) {
             animator.SetBool("Derecha", false);
         }
+
         //Movimiento hacia arriba
         if (Input.GetKey(KeyCode.UpArrow)) {
             transform.Translate(0, velocidad, 0);
@@ -43,5 +50,13 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.DownArrow)) {
             transform.Translate(0, -velocidad, 0);
         }
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("coin")) {
+            Destroy(other.gameObject);
+            coins++;
+
+        }
+
     }
 }
