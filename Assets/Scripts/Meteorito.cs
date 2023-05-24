c248979d232540d45ac428b0de8c402455a68b7f
AlbertoLyons <alberto.lyons@alumnos.ucn.cl>
2023-05-23 23:54:24 -0400

    fix(camara): Seguimiento del jugador

--- Assets/Scripts/Meteorito.cs
+++ Assets/Scripts/Meteorito.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Meteorito : MonoBehaviour
 {
+    public float rotacion = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,9 @@ public class Meteorito : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         transform.Rotate(0,0,0.0121f);
+
+         transform.Rotate(0,0,rotacion);
+
     }
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
