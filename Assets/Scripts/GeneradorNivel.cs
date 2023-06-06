d4bdadec48436b47017234f1ae3a96782f421f41
VicenteeFigueroa <vicente.figueroa01@alumnos.ucn.cl>
2023-06-06 14:37:14 -0400

    fix(plataformas): destruccion de prefabs en un determinado instante

--- Assets/Scripts/GeneradorNivel.cs
+++ Assets/Scripts/GeneradorNivel.cs
@@ -10,41 +10,53 @@ public class GeneradorNivel : MonoBehaviour
     [SerializeField] private int cantidadInicial;
     private Transform jugador;
 
+    private GameObject nivelActual;
 
-    void Start() {
+    void Start(){
         jugador = GameObject.FindGameObjectWithTag("Player").transform;
 
-        for (int i = 0; i < cantidadInicial; i++) {
+        for (int i = 0; i < cantidadInicial; i++){
             GenerarParteNivel();
         }
     }
+
     void Update() {
         float jugadorY = jugador.position.y;
         float puntoY = puntoFinal.position.y;
 
-        if (Mathf.Abs(jugadorY-puntoY) < distanciaMinima) {
-            for (int i = 0; i < 3; i++) {
+        if (Mathf.Abs(jugadorY - puntoY) < distanciaMinima){
+            for (int i = 0; i < 3; i++){
                 GenerarParteNivel();
-               
-             }
+
+            }
         }
     }
 
-    void GenerarParteNivel() {
-        Vector3 puntoY = new Vector3(-12.06f,puntoFinal.position.y-0.843264f);
+    void GenerarParteNivel(){
+        Vector3 puntoY = new Vector3(-12.06f, puntoFinal.position.y - 0.843264f);
         int numeroAleatorio = Random.Range(0, partesNivel.Length);
-        GameObject nivel = Instantiate(partesNivel[numeroAleatorio], puntoY, Quaternion.identity);
-        puntoFinal = BuscarPuntoFinal(nivel, "PuntoFinal");
+        nivelActual = Instantiate(partesNivel[numeroAleatorio], puntoY, Quaternion.identity);
+        puntoFinal = BuscarPuntoFinal(nivelActual, "PuntoFinal");
+
+        Camera.main.Render();
+
+        StartCoroutine(DestruirNivelDespuesDeDelay(nivelActual, 15f));
     }
 
-    Transform BuscarPuntoFinal(GameObject parteNivel, string etiqueta) {
+    Transform BuscarPuntoFinal(GameObject parteNivel, string etiqueta){
         Transform punto = null;
-        foreach (Transform ubicacion in parteNivel.transform) {
-            if (ubicacion.CompareTag(etiqueta)) {
+        foreach (Transform ubicacion in parteNivel.transform){
+            if (ubicacion.CompareTag(etiqueta)){
                 punto = ubicacion;
                 break;
             }
         }
         return punto;
+    }
+
+    IEnumerator DestruirNivelDespuesDeDelay(GameObject nivel, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(nivel);
     }
 }
