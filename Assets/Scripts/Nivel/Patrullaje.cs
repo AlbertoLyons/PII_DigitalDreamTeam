8e49126d0b5d3554374a4186c8513247aec9442d
VicenteeFigueroa <vicente.figueroa01@alumnos.ucn.cl>
2023-06-30 11:28:55 -0400

    fix(varios)

--- Assets/Scripts/Nivel/Patrullaje.cs
+++ Assets/Scripts/Nivel/Patrullaje.cs
@@ -7,8 +7,10 @@ public class Patrullaje : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     private Rigidbody2D m_rig;
+    public GameObject player;
     private float speed = 2f;
     public Transform childTransform;
+    public float fieldOfViewAngle;
 
     // Start is called before the first frame update
     void Awake()
@@ -26,7 +28,8 @@ public class Patrullaje : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other){
 
-      if(other.gameObject.tag == "Pasto"){
+      if(other.gameObject.tag == "Pasto")
+      {
         speed *= -1;
 
         childTransform.localPosition = new Vector3(-childTransform.localPosition.x, childTransform.localPosition.y, 0);
@@ -34,19 +37,69 @@ public class Patrullaje : MonoBehaviour
       }
 
       if (other.gameObject.CompareTag("Player"))
-    {
+      {
         // Dejar de atacar al jugador
         animator.SetBool("Attack", false);
-    }
+     }
 
     }
+
+    //float distanceX = transform.position.x - other.transform.position.x;
+    //float distanceY = transform.position.y - other.transform.position.y;
+
 
     private void OnTriggerEnter2D(Collider2D other){
 
-      if (other.gameObject.CompareTag("Player"))
-    {
+      float distanceX = transform.position.x - other.transform.position.x;
+      float distanceY = transform.position.y - other.transform.position.y;
+
+     if (other.gameObject.CompareTag("Player"))
+     {
         // Atacar al jugador
         animator.SetBool("Attack", true);
+        var playerTransform = other.gameObject.transform;
+
+        if (distanceX > fieldOfViewAngle && distanceY < 1)
+        {
+          if (transform.position.x > other.transform.position.x)
+          {
+            StartCoroutine(test(other, 250));
+          }
+        }else
+          {
+            StartCoroutine(test(other, -250));
+          }
+      }
     }
-}
+
+    private IEnumerator test(Collider2D other, int value){
+      yield return new WaitForSeconds(0.08f);
+      other.attachedRigidbody.AddForce(Vector2.left*value);
+    }
+
+
+    bool IsPlayerInFieldOfView()
+    {
+      if (player == null)
+      {
+          return false;
+      }
+      else
+      {
+        Vector3 direction = player.transform.position - transform.position;
+        float angle = Vector3.Angle(direction, transform.right);
+
+        if (angle <= fieldOfViewAngle * 0.5f)
+        {
+            // El jugador está dentro del campo de visión
+            return true;
+        }
+
+        // El jugador está fuera del campo de visión
+        return false;
+
+      }
+        
+    }
+
 }
